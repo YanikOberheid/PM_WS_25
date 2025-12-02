14231716b6be3604a654cc786d871df8cb3fd5aa
ZeLink11 <boogies.glut.0a@icloud.com>
2025-12-02 13:36:13 +0100

    initialisierung der JUnit-Test Klasse FensterUndAußenanlagen save/load

---
+++ src/test/SonderwuenscheFensterUndAußentuerenLadenUndSpeichernTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SonderwuenscheFensterUndAußentuerenLadenUndSpeichernTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
