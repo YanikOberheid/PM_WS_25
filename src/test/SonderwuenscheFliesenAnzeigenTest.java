d4c29a811566bfdb03b0d63b6263fc500969cb1a
AsselAZ <asselalzoabee@gmail.com>
2025-12-12 23:40:27 +0200

    parkett anzeigen fertig j unit test

--- src/test/SonderwuenscheFliesenAnzeigenTest.java
+++ src/test/SonderwuenscheFliesenAnzeigenTest.java
@@ -1,16 +1,14 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import gui.fliesen.FliesenView;
 import javafx.stage.Stage;
-
 
 /*
  * Hinweis: Fürs Holen der Ausgewhälten Sonderwünsche zu Fliesen ist die Klasse
@@ -22,9 +20,8 @@ import javafx.stage.Stage;
  * Nur FliesenView.updateFliesenCheckboxen(...) wird hier getestet.
  *  
  */
-	
+
 class SonderwuenscheFliesenAnzeigenTest {
-	
 
 	private Stage stage;
 	private FliesenView fliesenView;
@@ -44,47 +41,47 @@ class SonderwuenscheFliesenAnzeigenTest {
 	@Test
 	void anfangsAlleFalse() {
 		// Keine Checkbox sollte ausgewählt sein
-		for (boolean b: fliesenView.holeFliesenCheckboxenIsSelected())
+		for (boolean b : fliesenView.holeIsSelectedFuerCheckboxen())
 			assertFalse(b);
 	}
-	
+
 	@Test
 	void zweiNachAuswahlTrue() {
 		// 701 und 702 sind ausgewählt
-		fliesenView.updateFliesenCheckboxen(new int[] {701, 702});
-		assertTrue(fliesenView.holeFliesenCheckboxenIsSelected()[0]);
-		assertTrue(fliesenView.holeFliesenCheckboxenIsSelected()[1]);
+		fliesenView.updateSwCheckboxen(new int[] { 701, 702 });
+		assertTrue(fliesenView.holeIsSelectedFuerCheckboxen()[0]);
+		assertTrue(fliesenView.holeIsSelectedFuerCheckboxen()[1]);
 	}
-	
+
 	@Test
 	void nachNullAlleFalse() {
 		// 701 und 702 sind ausgewählt (aus Test zweiNachAuswahlTrue)
-		fliesenView.updateFliesenCheckboxen(new int[] {701, 702});
-		
+		fliesenView.updateSwCheckboxen(new int[] { 701, 702 });
+
 		// null sollte Auwahl aufheben
-		fliesenView.updateFliesenCheckboxen(null);
-		for (boolean b: fliesenView.holeFliesenCheckboxenIsSelected())
+		fliesenView.updateSwCheckboxen(null);
+		for (boolean b : fliesenView.holeIsSelectedFuerCheckboxen())
 			assertFalse(b);
 	}
-	
+
 	@Test
 	void alleTrue() {
 		// Alle Checkboxen auswählen
-		fliesenView.updateFliesenCheckboxen(new int[] {701, 702, 703, 704, 705, 706});
-		for (boolean b: fliesenView.holeFliesenCheckboxenIsSelected())
+		fliesenView.updateSwCheckboxen(new int[] { 701, 702, 703, 704, 705, 706 });
+		for (boolean b : fliesenView.holeIsSelectedFuerCheckboxen())
 			assertTrue(b);
 	}
-	
+
 	@Test
 	void nurNochZweiTrue() {
 		// Alle Checkboxen auswählen (aus Test alleTrue)
-		fliesenView.updateFliesenCheckboxen(new int[] {701, 702, 703, 704, 705, 706});
-		for (boolean b: fliesenView.holeFliesenCheckboxenIsSelected())
+		fliesenView.updateSwCheckboxen(new int[] { 701, 702, 703, 704, 705, 706 });
+		for (boolean b : fliesenView.holeIsSelectedFuerCheckboxen())
 			assertTrue(b);
-		
+
 		// Nur 703 und 705 auswählen
-		fliesenView.updateFliesenCheckboxen(new int[] {703, 705});
-		boolean[] b = fliesenView.holeFliesenCheckboxenIsSelected();
+		fliesenView.updateSwCheckboxen(new int[] { 703, 705 });
+		boolean[] b = fliesenView.holeIsSelectedFuerCheckboxen();
 		assertFalse(b[0]);
 		assertFalse(b[1]);
 		assertTrue(b[2]); // 703 an Index 2
