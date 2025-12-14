35f89d19541f2e074a9fd04d4c31d9cacffd5067
AsselAZ <asselalzoabee@gmail.com>
2025-12-14 20:12:22 +0200

    done

--- src/gui/fenster/FensterControl.java
+++ src/gui/fenster/FensterControl.java
@@ -1,12 +1,14 @@
 package gui.fenster;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Arrays;
-import java.util.Map;
 
 import business.kunde.KundeModel;
+import business.kunde.Sw;
+import business.kunde.SwKategorie;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import business.kunde.SwKategorie;
 
 /*
  * Klasse, welche das Fenster mit den Sonderwuenschen zu Fenstern und
@@ -14,119 +16,170 @@ import business.kunde.SwKategorie;
  */
 public final class FensterControl {
 
-    private FensterView fensterView;
-    private KundeModel kundeModel;
- 
-    public FensterControl() {
-        Stage stageFenster = new Stage();
-        stageFenster.initModality(Modality.APPLICATION_MODAL);
-        this.kundeModel = KundeModel.getInstance();
-        this.fensterView = new FensterView(this, stageFenster);
-    }
-  
-    public void oeffneFensterView() {
-        this.leseFensterSonderwuensche();
-        this.fensterView.oeffneFensterView();
-    }
+	private FensterView fensterView;
+	private KundeModel kundeModel;
 
-    public void leseFensterSonderwuensche() {
-        int[][] ausgewaehlteSw = kundeModel.gibAusgewaehlteSwMitAnzahlAusDb(
-        		SwKategorie.FENSTER_AUSSENTUEREN.id);
-        if (ausgewaehlteSw != null) {
-            this.fensterView.updateSwInView(ausgewaehlteSw);
-        }
-    }
-    
-    @Deprecated
-    public void speichereSonderwuensche(int[] fensterSw) {
-        int[] ausgewaehlteSw = this.kundeModel.gibAusgewaehlteSwAusDb();
-        if (ausgewaehlteSw == null) {
-            ausgewaehlteSw = new int[0];
-        }
+	public FensterControl() {
+		Stage stageFenster = new Stage();
+		stageFenster.initModality(Modality.APPLICATION_MODAL);
+		this.kundeModel = KundeModel.getInstance();
+		this.fensterView = new FensterView(this, stageFenster);
+	}
 
-        // Sonderwuensche zu Fenstern und Außentueren aus der alten Auswahl entfernen
-        // Fenster-Bereich: 300–399
-        ausgewaehlteSw = Arrays.stream(ausgewaehlteSw)
-                .filter(sw -> sw < 300 || sw >= 400)
-                .toArray();
+	public void oeffneFensterView() {
+		this.leseFensterSonderwuensche();
+		this.fensterView.oeffneFensterView();
+	}
 
-        // mit neuer Auswahl zusammenführen
-        int[] zuPruefendeSwKonstellation = new int[ausgewaehlteSw.length + fensterSw.length];
-        for (int i = 0; i < ausgewaehlteSw.length; i++) {
-            zuPruefendeSwKonstellation[i] = ausgewaehlteSw[i];
-        }
-        for (int i = 0; i < fensterSw.length; i++) {
-            zuPruefendeSwKonstellation[ausgewaehlteSw.length + i] = fensterSw[i];
-        }
+	public void leseFensterSonderwuensche() {
+		int[][] ausgewaehlteSw = kundeModel.gibAusgewaehlteSwMitAnzahlAusDb(SwKategorie.FENSTER_AUSSENTUEREN.id);
+		if (ausgewaehlteSw != null) {
+			this.fensterView.updateSwInView(ausgewaehlteSw);
+		}
+	}
 
-        // Konstellation pruefen (Logik kommt später, hier immer true)
-        if (this.pruefeKonstellationFensterAussentueren(zuPruefendeSwKonstellation)) {
-            try {
-                this.kundeModel.updateAusgewaehlteSw(zuPruefendeSwKonstellation);
-            } catch (Exception exc) {
-                System.out.println("Neue Auswahl an Sonderwuenschen zu Fenstern und Außentueren "
-                        + "konnte nicht gespeichert werden");
-            }
-        }
-    }
-    @Deprecated
-    public void speichereSonderwuensche(int[] fensterSw, int[][] fensterSwMitAnzahl) {
-    	// Erst Konstellation prüfen
-        if (!pruefeKonstellationFensterAussentueren(fensterSw, fensterSwMitAnzahl)) {
-            // Konflikt -> nicht speichern
-            return;
-        }
+	@Deprecated
+	public void speichereSonderwuensche(int[] fensterSw) {
+		int[] ausgewaehlteSw = this.kundeModel.gibAusgewaehlteSwAusDb();
+		if (ausgewaehlteSw == null) {
+			ausgewaehlteSw = new int[0];
+		}
 
-        try {
-            kundeModel.speichereSonderwuenscheFuerKategorie(
-            		fensterSw,
-            		fensterSwMitAnzahl,
-                    SwKategorie.FENSTER_AUSSENTUEREN.id
-            );
-        } catch (Exception e) {
-            System.out.println("Sonderwünsche zu Heizungen konnten nicht gespeichert werden.");
-            e.printStackTrace();
-        }
-    }
-    
-    // Aktuellste Version
- 	public void speichereSonderwuensche(int[][] fensterSwMitAnzahl) {
- 		// Erst Konstellation prüfen
-         if (!pruefeKonstellationFensterAussentueren(fensterSwMitAnzahl)) {
-             // Konflikt -> nicht speichern
-             return;
-         }
-         
-         try {
-             kundeModel.speichereSonderwuenscheFuerKategorie(
-            		 fensterSwMitAnzahl,
-                     SwKategorie.FENSTER_AUSSENTUEREN.id
-             );
-         } catch (Exception e) {
-             System.out.println("Sonderwünsche zu Fenster und Aussentueren-Varianten konnten nicht gespeichert werden.");
-             e.printStackTrace();
-         }
- 	}
-    
-    @Deprecated
-    public boolean pruefeKonstellationFensterAussentueren(int[] ids) {
-        return true;
-    }
-    @Deprecated
-    public boolean pruefeKonstellationFensterAussentueren(int[] ausgewaehlteSw,
-    		int[][] ausgewaehlteSwMitAnzahl) {
-        return true; // TODO
-    }
-    
-    // Aktuellste Version
- 	public boolean pruefeKonstellationFensterAussentueren(int[][] ausgewaehlteSwMitAnzahl){
- 		/* 
- 		 * - Prüfe, ob ein Sw in ausgewaehlteSw und ausgewaehlteSwMitAnzahl doppelt vorkommt.
- 		 * - Hole mit KundeModel.holeAusgewaehlteSwAusDbOhneKategorie() oder
- 		 * KundeModel.holeAusgewaehlteSwMitAnzahlAusDbOhneKategorie() die Auswahl der anderen
- 		 * Kategorien.
- 		 * - Prüfe, ob neue Auswahl mit der alten vereinbar ist und die Anzahlen erlaubt. 
- 		 */ 
- 		return true;
- 	}
+		// Sonderwuensche zu Fenstern und Außentueren aus der alten Auswahl entfernen
+		// Fenster-Bereich: 300–399
+		ausgewaehlteSw = Arrays.stream(ausgewaehlteSw).filter(sw -> sw < 300 || sw >= 400).toArray();
+
+		// mit neuer Auswahl zusammenführen
+		int[] zuPruefendeSwKonstellation = new int[ausgewaehlteSw.length + fensterSw.length];
+		for (int i = 0; i < ausgewaehlteSw.length; i++) {
+			zuPruefendeSwKonstellation[i] = ausgewaehlteSw[i];
+		}
+		for (int i = 0; i < fensterSw.length; i++) {
+			zuPruefendeSwKonstellation[ausgewaehlteSw.length + i] = fensterSw[i];
+		}
+
+		// Konstellation pruefen (Logik kommt später, hier immer true)
+		if (this.pruefeKonstellationFensterAussentueren(zuPruefendeSwKonstellation)) {
+			try {
+				this.kundeModel.updateAusgewaehlteSw(zuPruefendeSwKonstellation);
+			} catch (Exception exc) {
+				System.out.println("Neue Auswahl an Sonderwuenschen zu Fenstern und Außentueren "
+						+ "konnte nicht gespeichert werden");
+			}
+		}
+	}
+
+	@Deprecated
+	public void speichereSonderwuensche(int[] fensterSw, int[][] fensterSwMitAnzahl) {
+		// Erst Konstellation prüfen
+		if (!pruefeKonstellationFensterAussentueren(fensterSw, fensterSwMitAnzahl)) {
+			// Konflikt -> nicht speichern
+			return;
+		}
+
+		try {
+			kundeModel.speichereSonderwuenscheFuerKategorie(fensterSw, fensterSwMitAnzahl,
+					SwKategorie.FENSTER_AUSSENTUEREN.id);
+		} catch (Exception e) {
+			System.out.println("Sonderwünsche zu Heizungen konnten nicht gespeichert werden.");
+			e.printStackTrace();
+		}
+	}
+
+	// Aktuellste Version
+	public void speichereSonderwuensche(int[][] fensterSwMitAnzahl) {
+		// Erst Konstellation prüfen
+		if (!pruefeKonstellationFensterAussentueren(fensterSwMitAnzahl)) {
+			// Konflikt -> nicht speichern
+			return;
+		}
+
+		try {
+			kundeModel.speichereSonderwuenscheFuerKategorie(fensterSwMitAnzahl, SwKategorie.FENSTER_AUSSENTUEREN.id);
+		} catch (Exception e) {
+			System.out.println("Sonderwünsche zu Fenster und Aussentueren-Varianten konnten nicht gespeichert werden.");
+			e.printStackTrace();
+		}
+	}
+
+	@Deprecated
+	public boolean pruefeKonstellationFensterAussentueren(int[] ids) {
+		return true;
+	}
+
+	@Deprecated
+	public boolean pruefeKonstellationFensterAussentueren(int[] ausgewaehlteSw, int[][] ausgewaehlteSwMitAnzahl) {
+		return true; // TODO
+	}
+
+	// Aktuellste Version
+	public boolean pruefeKonstellationFensterAussentueren(int[][] ausgewaehlteSwMitAnzahl) {
+		/*
+		 * - Prüfe, ob ein Sw in ausgewaehlteSw und ausgewaehlteSwMitAnzahl doppelt
+		 * vorkommt. - Hole mit KundeModel.holeAusgewaehlteSwAusDbOhneKategorie() oder
+		 * KundeModel.holeAusgewaehlteSwMitAnzahlAusDbOhneKategorie() die Auswahl der
+		 * anderen Kategorien. - Prüfe, ob neue Auswahl mit der alten vereinbar ist und
+		 * die Anzahlen erlaubt.
+		 */
+		return true;
+	}
+
+	public static String erstelleCsvInhaltFenster(int[][] fensterSwMitAnzahl) {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("Sonderwunsch;Anzahl;Einzelpreis;Gesamtpreis\n");
+
+		if (fensterSwMitAnzahl == null) {
+			return sb.toString();
+		}
+
+		for (int[] sw : fensterSwMitAnzahl) {
+			if (sw == null || sw.length != 2)
+				continue;
+
+			Sw sonderwunsch = Sw.findeMitId(sw[0]);
+			int anzahl = sw[1];
+
+			double einzelpreis = sonderwunsch.preis;
+			double gesamtpreis = anzahl * einzelpreis;
+
+			sb.append(sonderwunsch.bes).append(";").append(anzahl).append(";").append(einzelpreis).append(";")
+					.append(gesamtpreis).append("\n");
+		}
+
+		return sb.toString();
+	}
+
+	public void exportiereSonderwuenscheAlsCsv(int[][] fensterSwMitAnzahl) {
+
+		try {
+			if (fensterSwMitAnzahl == null || fensterSwMitAnzahl.length == 0) {
+				System.out.println("Export: Keine Sonderwünsche zu Fenstern/Außentüren vorhanden.");
+				return;
+			}
+
+			if (kundeModel.getKunde() == null) {
+				System.out.println("Export fehlgeschlagen: Kein Kunde ausgewählt.");
+				return;
+			}
+
+			int kundennummer = kundeModel.getKunde().getIdKunde();
+			String nachname = kundeModel.getKunde().getNachname();
+
+			String dateiname = kundennummer + "_" + nachname + "_Fenster_Aussentueren.csv";
+			File file = new File(dateiname);
+
+			String csv = erstelleCsvInhaltFenster(fensterSwMitAnzahl);
+
+			try (FileWriter writer = new FileWriter(file)) {
+				writer.write(csv);
+			}
+
+			System.out.println("Export erfolgreich: " + file.getAbsolutePath());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Export fehlgeschlagen: Die CSV-Datei konnte nicht erstellt werden.");
+		}
+	}
 }
